fe8106c4edeb29a590baf52f85a13e0e4e553080
Curso <Curso@IPARTEKAULA.COM>
2020-02-27 13:44:08 +0100

    tareas

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,12 +7,47 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  public folder: string;
+  tareas: Array<any>;
+
+  constructor(private activatedRoute: ActivatedRoute) { 
+    console.trace('FolderPageModule constructor');
+    this.tareas = [
+      {
+        'id': 0,
+        'nombre': 'Primera tarea',
+        'completado': true
+      },
+      {
+        'id': 1,
+        'nombre': 'Segunda tarea',
+        'completado': false
+      },
+      {
+        'id': 2,
+        'nombre': 'Tercera tarea',
+        'completado': false
+      }
+    ]
+  }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
-}
+  modificar(tarea : any){
+    console.trace('FolderPageModule modificar');
+    tarea.completado = !tarea.completado
+  }
+
+  eliminar(tarea : any){
+    console.trace('FolderPageModule eliminar');
+    if( confirm("¿Estás seguro de que quieres eliminar la tarea?") ){
+
+    }else{
+      console.trace('Eliminación cancelada');
+    }
+  }
+
+}//fin FolderPageModule
